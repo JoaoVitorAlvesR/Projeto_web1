129c14fb3cb2c416a2039227613458af88258e26
João Vitor Alves Rocha <joaovitor.alvesr@outlook.com>
2022-05-27 17:58:16 -0300

    Login

--- Meu_amigo_pet/backend/controllers/UserController.js
+++ Meu_amigo_pet/backend/controllers/UserController.js
@@ -1,5 +1,9 @@
 const User = require("../models/User");
 
+const bcrypt = require("bcrypt");
+
+const createUserToken = require("../helpers/create-user-token");
+
 module.exports = class UserController {
   static async register(req, res) {
     const { name, email, phone, password, confirmpassword } = req.body;
@@ -10,7 +14,7 @@ module.exports = class UserController {
       return;
     }
 
-    if (!e-mail) {
+    if (!email) {
       res.status(422).json({ message: "O email é obrigatório" });
       return;
     }
@@ -27,21 +31,40 @@ module.exports = class UserController {
       res.status(422).json({ message: "A confirmação de senha é obrigatória" });
       return;
     }
-    if(password !== confirmpassword){
-      res.status(422).json({ message: 'A senha e a confirmação de senha precisam ser iguais'})
-      return
+    if (password !== confirmpassword) {
+      res.status(422).json({
+        message: "A senha e a confirmação de senha precisam ser iguais",
+      });
+      return;
     }
 
     // check if user exists
-    const userExists = await User.findOne({email: email})
+    const userExists = await User.findOne({ email: email });
 
-    if(userExists){
-      res.status(422).json({ 
-        message: 'Por favor, utilize outro e-mail',
-      })
-      return
+    if (userExists) {
+      res.status(422).json({
+        message: "Por favor, utilize outro e-mail",
+      });
+      return;
+    }
+
+    // create a password
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    // create a user
+    const user = new User({
+      name,
+      email,
+      phone,
+      password: passwordHash,
+    });
+
+    try {
+      const newUser = await user.save();
+      await createUserToken(newUser, req, res);
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
   }
-  //
-  
-}
-}
+};
