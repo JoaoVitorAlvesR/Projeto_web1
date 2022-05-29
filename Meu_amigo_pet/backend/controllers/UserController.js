9ce8ab49fe8e4118a8cdf5d61638b8a4f2d39733
João Vitor Alves Rocha <joaovitor.alvesr@outlook.com>
2022-05-29 16:13:43 -0300

    Validação de token e edição do usuário

--- Meu_amigo_pet/backend/controllers/UserController.js
+++ Meu_amigo_pet/backend/controllers/UserController.js
@@ -1,9 +1,10 @@
 const User = require("../models/User");
 
 const bcrypt = require("bcrypt");
-
+const jwt = require("jsonwebtoken");
+// helpers
 const createUserToken = require("../helpers/create-user-token");
-
+const getToken = require("../helpers/get-token");
 module.exports = class UserController {
   static async register(req, res) {
     const { name, email, phone, password, confirmpassword } = req.body;
@@ -68,9 +69,8 @@ module.exports = class UserController {
     }
   }
 
-  static async login(req, res){
-
-    const{email, password } = req.body
+  static async login(req, res) {
+    const { email, password } = req.body;
 
     if (!email) {
       res.status(422).json({ message: "O email é obrigatório" });
@@ -94,13 +94,57 @@ module.exports = class UserController {
     // check if password match with db password
     const checkPassword = await bcrypt.compare(password, user.password);
 
-    if(!checkPassword) {
+    if (!checkPassword) {
       res.status(422).json({
         message: "Senha inválida!",
       });
       return;
     }
 
-    await createUserToken(user, req, res)
+    await createUserToken(user, req, res);
+  }
+
+  static async checkUser(req, res) {
+    let currentUser;
+
+    console.log(req.headers.authorization);
+
+    if (req.headers.authorization) {
+
+      const token = getToken(req)
+      const decoded = jwt.verify(token, 'nossosecret')
+
+      currentUser = await User.findById(decoded.id)
+
+      currentUser.password = undefined
+
+    } else {
+      currentUser = null;
+    }
+
+    res.status(200).send(currentUser);
+  }
+
+  static async getUserById(req, res){
+
+    const id = req.params.id
+
+    const user = await User.findById(id).select("-password")
+
+    if(!user){
+      res.status(422).json({
+        message:'Usuário não encontrado',
+      })
+      return
+    }
+
+    res.status(200).json({ user })
+  }
+
+  static async editUser(req, res){
+    res.status(200).json({
+      message: 'Deu certo o update!',
+    })
+    return
   }
 };
