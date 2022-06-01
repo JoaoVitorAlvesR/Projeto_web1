0f29882afb7251995b8ad23a70e4f80d534ea6d1
rafavramos <engenheirorafaramos@gmail.com>
2022-06-01 18:44:58 -0300

    Front-End até a aula 274 concluida

--- Meu_amigo_pet/frontend/src/App.js
+++ Meu_amigo_pet/frontend/src/App.js
@@ -4,26 +4,29 @@ import Navbar from "./components/layout/Navbar";
 import Login from "./components/pages/Auth/Login";
 import Register from "./components/pages/Auth/Register";
 import Home from "./components/pages/Home";
-import Container from "./components/layout/Container"
+import Container from "./components/layout/Container";
+import { UserProvider } from "./context/UserContext";
 
 function App() {
   return (
     <Router>
-      <Navbar/>
-      <Container>
-      <Switch>
-        <Route path="/login">
-            <Login/>
-        </Route>
-        <Route path="/register">
-            <Register/>
-        </Route>
-        <Route path="/">
-            <Home/>
-        </Route>
-      </Switch>
-      </Container>
-      <Footer/>
+      <UserProvider>
+        <Navbar />
+        <Container>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Container>
+        <Footer />
+      </UserProvider>
     </Router>
   );
 }
--- Meu_amigo_pet/frontend/src/components/pages/Auth/Register.js
+++ Meu_amigo_pet/frontend/src/components/pages/Auth/Register.js
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import Input from "../../form/Input";
-import styles from "../../form/Form.module.css"
-import { Link } from "react-router-dom"
-
-
-function handleChange(e) {}
+import styles from "../../form/Form.module.css";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "../../../context/UserContext";
 
 function Register() {
+  const [user, setUser] = useState({});
+  const {register}=useContext(Context)
+
+  function handleChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    register(user)
+  }
+
   return (
     <section className={styles.form_container}>
       <h1>Registrar</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <Input
           text="Nome"
           type="text"
@@ -49,7 +61,7 @@ function Register() {
         <input type="submit" value="Cadastrar" />
       </form>
       <p>
-          Já tem conta ? <Link to="/login">Clique aqui.</Link>
+        Já tem conta ? <Link to="/login">Clique aqui.</Link>
       </p>
     </section>
   );
